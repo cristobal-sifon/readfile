2138c44c2b7820e50304a1a32022f0c18d0f68cb
cristobal-sifon <sifon@strw.leidenuniv.nl>
2014-09-13 21:24:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='readfile',
       version='1.0',
       description='A fliexible module to read ascii files',
       author='Cristobal Sifon',
       author_email='sifon@strw.leidenuniv.nl',
-      url='http://home.strw.leidenuniv.nl/~sifon/readfile/',
-      modules=['readfile'])
+      url='http://home.strw.leidenuniv.nl/~sifon/readfile/')#,
+#      modules=['readfile'])
