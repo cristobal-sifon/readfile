da962624957569e2fccc445be394923501c22d70
cristobal-sifon <sifon@strw.leidenuniv.nl>
2014-09-13 20:14:39 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='readfile',
+      version='1.0',
+      description='read ascii files flexibly',
+      author='Cristobal Sifon',
+      author_email='sifon@strw.leidenuniv.nl',
+      url='http://home.strw.leidenuniv.nl/~sifon/readfile/',
+      modules=['readfile'])
