9b763074509285c4bd0d1f3f9dce45b636b6b09b
cristobal-sifon <sifon@strw.leidenuniv.nl>
2014-09-13 21:33:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='readfile',
       description='A fliexible module to read ascii files',
       author='Cristobal Sifon',
       author_email='sifon@strw.leidenuniv.nl',
-      url='http://home.strw.leidenuniv.nl/~sifon/readfile/')#,
-#      modules=['readfile'])
+      url='http://home.strw.leidenuniv.nl/~sifon/readfile/',
+      modules=['readfile'])
