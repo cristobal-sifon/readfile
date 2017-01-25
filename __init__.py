63c41c294906b36e166cddab6638ddc7ab156397
cristobal-sifon <sifon@strw.leidenuniv.nl>
2017-01-25 14:04:59 +0100

    fixed version string

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@ A flexible module to read ascii files
 
 """
 
-__all__ = ('dict', 'header', 'table', '_append_single_line')
-__version__ = '1.0'
+__all__ = ('dict', 'header', 'table', 'save', '_append_single_line')
+__version__ = '1.1.1'
--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
+import re
 from setuptools import setup
 
+
+#Taken from the Python docs:
+#Utility function to read the README file.
+#Used for the long_description.  It's nice, because now 1) we have a
+#top level README file and 2) it's easier to type in the README file
+#than to put a raw string in below
+def read(fname):
+    return open(os.path.join(here, fname)).read()
+
+
+#this function copied from pip's setup.py
+#https://github.com/pypa/pip/blob/1.5.6/setup.py
+#so that the version is only set in the __init__.py and then read here
+#to be consistent
+def find_version(fname):
+    version_file = read(fname)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(name='readfile',
-      version='1.1.0',
+      version=find_version('__init__.py'),
       description='A flexible module to read ascii files',
       author='Cristobal Sifon',
       author_email='sifon@astro.princeton.edu',
