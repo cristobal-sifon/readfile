c527f74e11f863a51873344ce5d029e6cd565dd4
Cristobal Sifon <sifon@astro.princeton.edu>
2018-01-04 17:23:19 -0500

    implement setup_helpers in v2.1.1

--- setup.py
+++ setup.py
@@ -2,28 +2,10 @@ import os
 import re
 from setuptools import setup
 
+from setup_helpers import find_version, read
+
+
 _here = os.path.abspath(os.path.dirname(__file__))
-
-#Taken from the Python docs:
-#Utility function to read the README file.
-#Used for the long_description.  It's nice, because now 1) we have a
-#top level README file and 2) it's easier to type in the README file
-#than to put a raw string in below
-def read(fname):
-    return open(os.path.join(_here, fname)).read()
-
-
-#this function copied from pip's setup.py
-#https://github.com/pypa/pip/blob/1.5.6/setup.py
-#so that the version is only set in the __init__.py and then read here
-#to be consistent
-def find_version(fname):
-    version_file = read(fname)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
 
 
 setup(
@@ -32,7 +14,7 @@ setup(
     description='A flexible module to read ascii files',
     author='Cristobal Sifon',
     author_email='sifon@astro.princeton.edu',
-    long_description=read('README.md'),
+    long_description=read(os.path.join(_here, 'README.md')),
     url='https://github.com/cristobal-sifon/readfile',
     packages=['readfile'],
     install_requires=['numpy>=1.5.0'],
