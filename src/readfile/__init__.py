cdb3a0018c56d6cd61375408ca73ff674fdd72e4
Cristóbal Sifón <cristobal.sifon@pucv.cl>
2020-12-11 17:31:58 -0300

    fixed __all__

--- src/readfile/__init__.py
+++ src/readfile/__init__.py
@@ -7,7 +7,6 @@ from __future__ import absolute_import
 from .readfile import *
 
 
-__all__ = ('dict', 'format_fmt', 'header', 'table', 'save',
-           '_append_single_line')
+__all__ = ('dict', 'format_fmt', 'header', 'table', 'save')
 __author__ = 'Cristobal Sifon'
 __version__ = '2.1.4'
